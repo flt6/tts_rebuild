694d795da19a3090730e16a362a308aecbd9b35d
flt6 <42725841+flt6@users.noreply.github.com>
2022-11-21 15:07:26 +0000

    🐞 fix(setup.py): fix requires

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 setup(
     name="my-azure-tts",
     version="0.0.1",
-    requires=["pydub","websockets","requests"],
+    install_requires=["pydub","websockets","requests"],
     # download_url="",
     packages=["mytts"]
 )
